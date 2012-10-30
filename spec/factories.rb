1aac175f9415cbf6d91b8128ea637a8315101ad0
Kristian Freeman <kristian@kristianfreeman.com>
2012-10-30 12:00:08 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Kristian Freeman"
+    email "kristian@kristianfreeman.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
